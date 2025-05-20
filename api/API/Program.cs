4ce6aef6480ec42e3067492749a3184c0f6dcf8a
Ngô Minh Quang <quangnm@phucthanhgroup.com>
2025-05-20 21:52:36 +0700

    up

--- api/API/Program.cs
+++ api/API/Program.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using log4net.Config;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -7,18 +6,13 @@ namespace API
 {
     public class Program
     {
-        private static readonly log4net.ILog logQ =
-    log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static void Main(string[] args)
         {
-            XmlConfigurator.Configure(new FileInfo("log4net.config"));
             CreateHostBuilder(args).Build().Run();
-            logQ.Info("Application started");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                //.UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory())
